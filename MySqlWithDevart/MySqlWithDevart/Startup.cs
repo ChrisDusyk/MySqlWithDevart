436af76da31e5c9cc76d665b2f3ccd4b2140e435
Chris Dusyk <chris.dusyk@gmail.com>
2016-06-07 10:56:52 -0600

    Created initial solution

---
+++ MySqlWithDevart/MySqlWithDevart/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MySqlWithDevart.Startup))]
+
+namespace MySqlWithDevart
+{
+	public partial class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			ConfigureAuth(app);
+		}
+	}
+}
